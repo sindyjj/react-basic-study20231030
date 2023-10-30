feeb52b5effba96c88468dd3261d839a6acbc8df
joungwook <sindyjj1@naver.com>
2023-10-30 15:55:07 +0900

    2023.10.23  1. React 초기 설정 완료 및  component 개념 확인

---
+++ src/NoName.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NoName extends React.Component {
+  render() {
+    return (
+      <div className='wrap'>
+        <h1>승건 하이</h1>
+      </div>
+    );
+  }
+}
+
+export default NoName;
